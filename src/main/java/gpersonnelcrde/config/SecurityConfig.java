7a0ba4bc1f34132c5261b232df52a3207e1fed98
Judicael Ouikon Dongongo <j.ouikon-ext@groupeonepoint.com>
2025-09-13 19:57:59 -0400

    Ajout de secrutité basique avec identifiants et pwds hardcodés

---
+++ src/main/java/gpersonnelcrde/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package gpersonnelcrde.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	public final static Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService(){
+		UserDetails user = User.builder()
+					.username("crde")
+					.password(passwordEncoder().encode("user123"))
+					.roles("USER")
+					.build();
+
+		UserDetails admin = User.builder()
+					.username("admin")
+					.password(passwordEncoder().encode("admin123"))
+					.roles("USER", "ADMIN")
+					.build();
+		logger.info("user pwd : {}", passwordEncoder().encode("user123"));
+		logger.info("admin pwd : {}", passwordEncoder().encode("admin123"));
+
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	} 
+}
